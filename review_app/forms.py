882069c204a21bf57b08746a0ab4f64a0e78b39d
victoria makena <makenavictoria1@gmail.com>
2022-06-12 20:32:53 +0300

    forms: add project form

--- review_app/forms.py
+++ review_app/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Profile
+from .models import Profile, Project
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('bio', 'profile_picture')
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields=('title','image' ,'description','git_url' )
